deb141b13681df7d7af5d6c3f226e90de1766570
Santi Arocha <arochas846@gmail.com>
2024-11-04 12:18:15 -0300

    LogIn/Out

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import userModel from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-import jwt from "jsonwebtoken";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
     const data = req.body;
@@ -18,17 +18,9 @@ export const register = async (req, res) => {
         );
         const savedUser = await newUser.save();
 
-        jwt.sign({
-            id: savedUser._id
-        },
-        "secretkey1234",
-        {
-            expiresIn:"1d"
-        },
-        (error, token) => {
-            error ? console.log("error: ", error) : res.cookie("token", token) && res.json({message:"Usuario creado"})
-        }
-    );
+        const token = await createAccessToken({id: savedUser._id});
+
+        res.cookie("token", token) && res.json({message:"Usuario creado"})
 
     } catch (error) {
         console.log("Error: ", error);
@@ -36,6 +28,44 @@ export const register = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send("Sesión iniciada");
+export const login = async (req, res) => {
+    const data = req.body;
+    const userInfo  = {email:data.email, password:data.password };
+
+    console.log("data que envio", userInfo);
+    
+    try {
+        
+        const user = userInfo.email;
+        const userPass = userInfo.password;
+
+        console.log("data que paso", user);
+
+        const userFound = await userModel.findOne({email:user});
+        
+        console.log("back response: ",userFound);
+
+        if (!userFound) {
+            return res.status(400).json({messaje:"user not found"})
+        }
+        else {
+            const isMatch = await bcryptjs.compare(userPass, userFound.password);
+
+            if(!isMatch) {
+                return res.status(400).json({messaje:"Incorrecta passsword"})
+            }
+            else{
+                const token = await createAccessToken({id: userFound._id});
+                res.cookie("token", token) && res.json({message:"Sesión iniciada"});
+            }
+        }
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(400).send("Error en el inicio");
+    }
+};
+
+export const logout = (req, res) => {
+    res.cookie('token', '', {expires : new Date(0)});
+    return res.status(200).json("Sesión cerrada");
 };
